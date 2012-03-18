aca183eae5cff64a0bf47f0e10ce27a03297f77c
Laszlo Borsos <borsosl@gmail.com>
2012-03-18 23:11:48 +0100

    Ed codepage to Unicode

---
+++ src/hu/vidyavana/convert/ed/EdCharacter.java
@@ -0,0 +1,79 @@
+package hu.vidyavana.convert.ed;
+
+
+public class EdCharacter
+{
+	// ed--unicode pairs
+	static int[] pair = {
+		0xa7, 'á',
+		0xa6, 'é',
+		0xa8, 'í',
+		0xa9, 'ó',
+		0xa2, 'ö',
+		0xc6, 'ő',
+		0xcd, 'ú',
+		0xa4, 'ü',
+		0xc9, 'ű',
+		
+		0xb7, 'Á',
+		0xb6, 'É',
+		0xb8, 'Í',
+		0xb9, 'Ó',
+		0xb2, 'Ö',
+		0xd6, 'Ő',
+		0xdd, 'Ú',
+		0xb4, 'Ü',
+		0xd9, 'Ű',
+		
+		0x83, 'ā',
+		0x8c, 'ī',
+		0x81, 'ū',
+		0x88, 'ḍ',
+		0x8b, 'ḥ',
+		0x8e, 'ḷ',
+		0x8f, 'ḹ',
+		0x84, 'ṁ',
+		0x85, 'ṅ',
+		0x86, 'ṇ',
+		0x8a, 'ñ',
+		0x82, 'ṛ',
+		0x8d, 'ṝ',
+		0x89, 'ṣ',
+		0x80, 'ś',
+		0x87, 'ṭ',
+		
+		0x93, 'Ā',
+		0x9c, 'Ī',
+		0x91, 'Ū',
+		0x98, 'Ḍ',
+		0x9b, 'Ḥ',
+		0x9e, 'Ḷ',
+		0x9f, 'Ḹ',
+		0x94, 'Ṁ',
+		0x95, 'Ṅ',
+		0x96, 'Ṇ',
+		0x9a, 'Ñ',
+		0x92, 'Ṛ',
+		0x9d, 'Ṝ',
+		0x99, 'Ṣ',
+		0x90, 'Ś',
+		0x97, 'Ṭ',
+
+		0xc0, '„'
+		// „“”‚‘’–—
+	};
+
+	static int[] table = new int[128];
+	
+	static
+	{
+		for(int i=0; i<pair.length; i+=2)
+			table[pair[i]-128] = pair[i+1];
+	}
+
+	
+	public static int convert(int c)
+	{
+		return table[c-128];
+	}
+}
--- src/hu/vidyavana/convert/ed/EdFileProcessor.java
+++ src/hu/vidyavana/convert/ed/EdFileProcessor.java
@@ -13,28 +13,43 @@ public class EdFileProcessor
 	private void readEdFile(File ed) throws Exception
 	{
 		InputStream is = new BufferedInputStream(new FileInputStream(ed));
-		byte[] sor = new byte[1000];
+		// use short instead of byte to work around signed byte handling
+		short[] line = new short[1000];
 		int ptr = 0;
 		while(true)
 		{
 			int c = is.read();
 			if(c==10 || c<0)
 			{
-				while(ptr>0 && sor[ptr-1]==32) --ptr;
+				while(ptr>0 && line[ptr-1]==32) --ptr;
 				if(ptr > 0)
-					processLine(sor, ptr);
+					processLine(line, ptr);
 				if(c < 0) break;
 				ptr = 0;
 				continue;
 			}
 			if(c==13 || ptr==0 && c==32) continue;
-			sor[ptr++] = (byte) c;
+			line[ptr++] = (short) c;
 		}
 	}
 	
 	
-	private void processLine(byte[] sor, int length)
+	private void processLine(short[] line, int length)
 	{
+		// convert ed encoding to unicode
+		StringBuffer sb = new StringBuffer();
+		for(int i=0; i<length; ++i)
+		{
+			// byte is signed!
+			if(line[i]>=128)
+			{
+				int c = EdCharacter.convert(line[i]);
+				if(c != 0)
+					sb.append((char) c);
+			}
+			else
+				sb.append((char) line[i]);
+		}
 	}
 
 
