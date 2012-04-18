888efa8f5147893835d83ef06e339e49592eea1f
Laszlo Borsos <borsosl@gmail.com>
2012-04-18 17:59:58 +0200

    Ed tag grouping 2

--- src/hu/vidyavana/convert/api/Align.java
+++ src/hu/vidyavana/convert/api/Align.java
@@ -2,5 +2,27 @@ package hu.vidyavana.convert.api;
 
 public enum Align
 {
-	Left, Center, Right, Justify, Verse;
+	Left,
+	Center,
+	Right,
+	Justify,
+	Verse;
+	
+
+	public final char abbrev;
+
+	
+	Align()
+	{
+		this.abbrev = Character.toLowerCase(name().charAt(0));
+	}
+	
+	
+	public static Align get(char abbrev)
+	{
+		for(Align a : values())
+			if(abbrev == Character.toLowerCase(a.name().charAt(0)))
+				return a;
+		return null;
+	}
 }
--- src/hu/vidyavana/convert/api/ParagraphStyle.java
+++ src/hu/vidyavana/convert/api/ParagraphStyle.java
@@ -1,32 +1,60 @@
 package hu.vidyavana.convert.api;
 
+import java.util.List;
+
 public class ParagraphStyle
 {
 	// see enum
 	public Align align;
 	
 	// relative font size compared to user font setting. Percentage, 100=base font size
-	public int font = 100;
+	public Integer size;
 	
+	// indents and pushes
 	// relative measurement compared to user font setting. 100=1 line.
-	public int indent;
-	public int before;
-	public int after;
+	public Integer left;
+	public Integer right;
+	public Integer before;
+	public Integer after;
+	public Integer first;
+	public Integer hanging;
+	
+	// tabs in percentage of the screen width
+	public List<Integer> tabs;
 	
 	
 	@Override
 	public String toString()
 	{
 		StringBuilder sb = new StringBuilder();
-		sb.append(align.toString());
-		if(font != 100)
-			sb.append("_f").append(font);
-		if(indent != 0)
-			sb.append("_i").append(indent);
-		if(before != 0)
+		
+		if(align != null)
+			sb.append("_i").append(align.abbrev);
+		if(size != null)
+			sb.append("_s").append(size);
+		if(left != null)
+			sb.append("_l").append(left);
+		if(right != null)
+			sb.append("_r").append(right);
+		if(before != null)
 			sb.append("_b").append(before);
-		if(after != 0)
+		if(after != null)
 			sb.append("_a").append(after);
+		if(first != null)
+			sb.append("_f").append(first);
+		if(hanging != null)
+			sb.append("_h").append(hanging);
+		if(tabs != null)
+			for(Integer i : tabs)
+				sb.append("_t").append(i);
 		return sb.toString();
+	}
+	
+	
+	public static ParagraphStyle fromString(String style)
+	{
+		ParagraphStyle s = new ParagraphStyle();
+		// TODO parse
+		return s ;
 	}
 }
